84330486faa88d7b3ea815741f80bad9845394c7
ysl2 <www.songli.yu@gmail.com>
2024-02-28 11:19:06 +0800

    Finish deploy yunet_lung

--- _.sh
+++ _.sh
@@ -8,41 +8,50 @@ export RESULTS_FOLDER="${nnUNet_dataset}/nnUNet_results"
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
 
 
-TASK=002
-
-
 function preprocess () {
+    task="$1"
+
     nnUNet_plan_and_preprocess \
-        -t "${TASK}" \
+        -t "$task" \
         --planner3d ExperimentPlannerTransUNet3D \
-        --planner2d ExperimentPlannerTransUNet
+        --planner2d ExperimentPlannerTransUNet \
+        -no_pp
 }
 
 
 function train () {
-    CUDA=0
-    FOLD=0
-    TRAINER=TransUNetTrainer_yusongli
+    cuda="$1"
+    trainer="$2"
+    task="$3"
+    fold="$4"
 
-    CUDA_VISIBLE_DEVICES=${CUDA} \
+    CUDA_VISIBLE_DEVICES=$cuda \
     nnUNet_train \
         TransUNet3D \
-        "${TRAINER}" \
-        "${TASK}" \
-        "${FOLD}" \
+        "$trainer" \
+        "$task" \
+        "$fold" \
         --npz
 }
 
 
 function main () {
-    preprocess
+    # preprocess
 
     # FOLDS=(0 1 2 3 4)
     # for i in ${FOLDS[@]}; do
     #     train "${i}"
     # done
 
-    # train
+    trainer=TransUNetTrainer_yusongli
+    task=203
+
+    # preprocess "$task"
+    train 5 "$trainer" "$task" 0 &
+    train 6 "$trainer" "$task" 1 &
+    train 7 "$trainer" "$task" 2 &
+    train 5 "$trainer" "$task" 3 &
+    train 6 "$trainer" "$task" 4 &
 }
 
 
